43b32410a25e7b7028c25c0558bf4dfb9d828ebd
Vladislav <newvlad2001@gmail.com>
2021-03-30 17:39:24 +0300

    size checking fix

--- main.c
+++ main.c
@@ -1,3 +1,9 @@
+/*Написать программу, находящую в заданном каталоге и всех его подкаталогах все файлы,
+ * заданного размера. Имя каталога задаётся пользователем в качестве первого аргумента командной строки.
+ * Диапазон от N1 до N2 задается в аргументах командной строки. Программа выводит результаты поиска в файл
+ * (четвертый аргумент командной строки) в виде полный путь, имя файла, его размер. На консоль выводится
+ * общее число просмотренных файлов.*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <dirent.h>
@@ -37,11 +43,10 @@ int main(int argc, char *argv[]) {
         print_error(program_name, "Can't open file:", argv[4]);
         return 1;
     }
-    fprintf(to_write, "%-100s     %-30s     %-10s\n", "PATH:", "NAME:", "SIZE:");
     find_files(dir_name, atoi(argv[2]), atoi(argv[3]));
     if (fclose(to_write) == -1)
         print_error(program_name, "Error closing file:", realpath(argv[2], NULL));
-    printf("Amount of checked files: %d\n", counter);
+    printf("%d\n", counter);
     return 0;
 
 }
@@ -61,6 +66,7 @@ void find_files(char *root_path, int from_size, int to_size) {
     DIR *curr_dir;
     if ((curr_dir = opendir(root_path)) == NULL) {
         print_error(program_name, strerror(errno), NULL);
+        return;
     }
     while ((structdirent = readdir(curr_dir))) {
         path = get_full_path(root_path, structdirent->d_name);
@@ -72,12 +78,15 @@ void find_files(char *root_path, int from_size, int to_size) {
             if (stat(path, buf) == -1) {
                 print_error(program_name, strerror(errno), NULL);
             }
-            if (buf->st_size > from_size && buf->st_size <= to_size) {
-                fprintf(to_write, "%-100s     %-30s     %-10ld\n", path, structdirent->d_name, buf->st_size);
+            if (buf->st_size > from_size && buf->st_size < to_size) {
+                fprintf(to_write, "%s %s %ld\n", path, structdirent->d_name, buf->st_size);
             }
             counter++;
         }
+        errno = 0;
     }
+    if (errno != 0)
+        print_error(program_name, strerror(errno), NULL);
     if (closedir(curr_dir) == -1) {
         print_error(program_name, strerror(errno), NULL);
     }
